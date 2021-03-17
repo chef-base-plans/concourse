3da8d85932e32d0cc2d56757fa9da1ffa3ffe227
Lucas Giancola <lucas.giancola@live.com>
2021-03-17 11:04:27 -0400

    Bumped version "4.2.2" -> "4.2.3"

--- plan.sh
+++ plan.sh
@@ -1,13 +1,13 @@
 pkg_name=concourse
 pkg_origin=core
-pkg_version="4.2.2"
+pkg_version="4.2.3"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('Apache-2.0')
 pkg_description="CI that scales with your project"
 pkg_upstream_url="https://concourse.ci"
 pkg_source="https://github.com/${pkg_name}/${pkg_name}/releases/download/v${pkg_version}/${pkg_name}_linux_amd64"
 pkg_filename="${pkg_name}_linux_amd64"
-pkg_shasum="5180903fee6a8fcdf8ba5bd34f270c5c467342f45534692af16f2951181e8749"
+pkg_shasum="02a91365131aaa87039c95d79dd898fb4b6cde3c55e6545a442e09b5fc809d23"
 
 pkg_deps=(
   core/glibc
